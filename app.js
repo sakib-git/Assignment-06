d97cffcc07a434bfcb805b4514e5a6a80b2c9df0
sakib-git <sakibulhasan32002@gmail.com>
2025-09-07 09:57:00 +0600

    navbar completed

---
+++ app.js
@@ -0,0 +1,73 @@
+const navContainer = document.getElementById('navContainer');
+
+const mainAllTress = document.getElementById('mainAllTress');
+const loadNav = () => {
+  const url = 'https://openapi.programming-hero.com/api/categories';
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      // console.log(data.categories)
+      showNav(data.categories);
+    });
+};
+
+const showNav = (categories) => {
+  // console.log(categories)
+  categories.forEach((categorie) => {
+    // console.log(categorie)
+    const div = document.createElement('div');
+    div.innerHTML = `
+ <li> ${categorie.category_name}</li>
+   `;
+    navContainer.append(div);
+  });
+  navContainer.addEventListener('click', (e) => {
+    // console.log(e)
+
+    const allLi = document.querySelectorAll('li');
+
+    allLi.forEach((li) => {
+      li.classList.remove('bg-[#15803D]', 'text-white', 'p-1', 'rounded-md');
+    });
+    if (e.target.localName === 'li') {
+      e.target.classList.add('bg-[#15803D]', 'text-white', 'p-1', 'rounded-md');
+    }
+  });
+};
+
+
+const allTrees = () => {
+  const url = 'https://openapi.programming-hero.com/api/plants';
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      showTrees(data.plants);
+    });
+};
+
+const showTrees = (plants) => {
+  // console.log(plants)
+  plants.forEach((plant) => {
+    // console.log(plant);
+    const div = document.createElement('div');
+    div.innerHTML = `
+<div class='flex flex-col gap-2 p-3 bg-white shadow-md rounded-md'>
+
+      <img src="${plant.image}" alt="${plant.name}" class="w-full h-80 rounded-lg"/>
+    <h2 class='text-2xl'>${plant.name}</h2>
+    <p class='line-clamp-3'>${plant.description}</p>
+<div class='flex justify-between'>
+    <P>${plant.category}</P>
+    <P>${plant.price}</P>
+</div>
+    <button class='bg-[#15803D] text-white p-2 rounded-full font-medium'>add to cart</button>
+
+
+</div>
+
+    `;
+    mainAllTress.append(div);
+  });
+};
+allTrees();
+loadNav();
